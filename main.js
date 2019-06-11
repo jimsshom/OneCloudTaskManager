918ee3d38c2345177ae1f59b99759c9f5a8787c7
jimsshom <jimsshom@gmail.com>
2019-06-11 23:41:04 +0800

    初步样式调通

--- main.js
+++ main.js
@@ -4,14 +4,30 @@ const {app, BrowserWindow, Tray, Menu, nativeImage, ipcMain} = require('electron
 let mainWindow
 
 function createMainWindow() {
+    let tmpX = 800
+    let tmpY = 400
+    let debug = true
+
+    let displays = electron.screen.getAllDisplays()
+    let externalDisplay = displays.find((display) => {
+        return display.bounds.x !== 0 || display.bounds.y !== 0
+    })
+    if (externalDisplay) {
+        tmpX += externalDisplay.bounds.x
+        tmpY += externalDisplay.bounds.y
+    }
     mainWindow = new BrowserWindow({
+        x: tmpX,
+        y: tmpY,
         show: false,
         frame: true,
         webPreferences: {
             nodeIntegration: true
         }
     })
-    mainWindow.webContents.openDevTools()
+    if (debug) {
+        mainWindow.webContents.openDevTools()
+    }
     mainWindow.loadFile('main.html')
     mainWindow.once('ready-to-show', () => {
         mainWindow.show()
